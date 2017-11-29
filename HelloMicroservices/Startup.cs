6b544fd8c4d28b5f41dac93f1f6fd51c4adad35d
aj.jammy <aj.jammy09@gmail.com>
2017-11-30 01:11:20 +0700

    Complete Chapter01

--- HelloMicroservices/Startup.cs
+++ HelloMicroservices/Startup.cs
@@ -1,36 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-
 namespace HelloMicroservices
 {
+    using Microsoft.AspNetCore.Builder;
+    using Nancy.Owin;
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void Configure(IApplicationBuilder app)
         {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
+            app.UseOwin(buildFunc =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+                buildFunc(next => env =>
+                {
+                    System.Console.WriteLine("Got request");
+                    return next(env);
+                });
+                buildFunc.UseNancy();
             });
         }
     }
