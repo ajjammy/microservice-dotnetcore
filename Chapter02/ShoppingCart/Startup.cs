2d34cc603ee89b610ea81cbc56efcf4ab1ef51d6
aj.jammy <aj.jammy09@gmail.com>
2017-12-02 22:50:29 +0700

    Chapter02 create shopping cart project

---
+++ Chapter02/ShoppingCart/Startup.cs
@@ -0,0 +1,13 @@
+namespace ShoppingCart
+{
+    using Microsoft.AspNetCore.Builder;
+    using Nancy.Owin;
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseOwin(x => x.UseNancy());
+        }
+    }
+}
